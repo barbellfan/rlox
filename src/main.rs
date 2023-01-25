68741a3828100b1fd1ec7eb8a342c669ad663bd7
Chris Allen <gh.84kjuqg365@gmail.com>
2023-01-24 19:42:20 -0800

    Basic main() method. Add tasks.json.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    if args.len() > 2 {
+        println!("Usage: jlox [script]");
+        process::exit(64);
+    } else if args.len() == 2 {
+        let path = &args[1];
+        run_file(path);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &String) {
+    println!("path: {}", path);
+}
+
+fn run_prompt() {
+    println!("running prompt");
 }
