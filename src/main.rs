e559785549babb1ea44f8667c014723e8039936b
Chris Allen <gh.84kjuqg365@gmail.com>
2023-01-24 20:53:38 -0800

    Load a file and dump it to stdout as a byte array,

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,16 +10,23 @@ fn main() {
         process::exit(64);
     } else if args.len() == 2 {
         let path = &args[1];
-        run_file(path);
+        run_file(path).unwrap();
     } else {
         run_prompt();
     }
 }
 
-fn run_file(path: &String) {
-    println!("path: {}", path);
+fn run_file(path: &String) -> Result<(), std::io::Error> {
+    let bytes = fs::read(path).unwrap();
+    run(bytes)
 }
 
 fn run_prompt() {
     println!("running prompt");
+}
+
+fn run(bytes: Vec<u8>) -> Result<(), std::io::Error> {
+    dbg!("{}", bytes);
+
+    Ok(())
 }
