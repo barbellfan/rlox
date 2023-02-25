8f8fde3aea7cac536ee5bf99c5d46c68dbce14d4
Chris Allen <gh.84kjuqg365@gmail.com>
2023-02-25 14:09:46 -0800

    Scan more token types Add had_error field to scanner. Not really used yet. Fix data type of line in LoxError. Fix add_token() to accept string for token, not char.

--- src/lox_scanner.rs
+++ src/lox_scanner.rs
@@ -8,12 +8,13 @@ pub struct Scanner {
     tokens: Vec<Token>,
     start: usize, // Location in array. Use usize to match array index data type.
     current: usize, // ditto
-    line: u32 // Line number. Use u32 since there can be lots of lines in a file.
+    line: u32, // Line number. Use u32 since there can be lots of lines in a file.
+    had_error: bool
 }
 
 #[derive(Debug, Clone)]
 pub struct LoxError {
-    line: i64,
+    line: u32,
     error_type: String,
     error_message: String
 }
@@ -31,7 +32,8 @@ impl Scanner {
             tokens: vec!(),
             start: 0,
             current: 0,
-            line: 1
+            line: 1,
+            had_error: false
         }
     }
 }
@@ -54,17 +56,35 @@ fn is_at_end(scanner: &Scanner) -> bool {
 fn scan_token(scanner: &mut Scanner) {
     let c = advance(scanner);
     match c {
-        '(' => add_token(scanner, TokenType::LEFT_PAREN, c),
-        ')' => add_token(scanner, TokenType::RIGHT_PAREN, c),
-        '{' => add_token(scanner, TokenType::LEFT_BRACE, c),
-        '}' => add_token(scanner, TokenType::RIGHT_BRACE, c),
-        ',' => add_token(scanner, TokenType::COMMA, c),
-        '.' => add_token(scanner, TokenType::DOT, c),
-        '-' => add_token(scanner, TokenType::MINUS, c),
-        '+' => add_token(scanner, TokenType::PLUS, c),
-        ';' => add_token(scanner, TokenType::SEMICOLON, c),
-        '*' => add_token(scanner, TokenType::STAR, c),
-        other => add_token(scanner, TokenType::STRING, other)
+        '(' => add_token(scanner, TokenType::LEFT_PAREN, c.to_string()),
+        ')' => add_token(scanner, TokenType::RIGHT_PAREN, c.to_string()),
+        '{' => add_token(scanner, TokenType::LEFT_BRACE, c.to_string()),
+        '}' => add_token(scanner, TokenType::RIGHT_BRACE, c.to_string()),
+        ',' => add_token(scanner, TokenType::COMMA, c.to_string()),
+        '.' => add_token(scanner, TokenType::DOT, c.to_string()),
+        '-' => add_token(scanner, TokenType::MINUS, c.to_string()),
+        '+' => add_token(scanner, TokenType::PLUS, c.to_string()),
+        ';' => add_token(scanner, TokenType::SEMICOLON, c.to_string()),
+        '*' => add_token(scanner, TokenType::STAR, c.to_string()),
+        '!' if is_token_match(scanner, '=') => add_token(scanner, TokenType::BANG_EQUAL, "!=".to_string()),
+        '!' => add_token(scanner, TokenType::BANG, c.to_string()),
+        '=' if is_token_match(scanner, '=') => add_token(scanner, TokenType::EQUAL_EQUAL, "==".to_string()),
+        '=' => add_token(scanner, TokenType::EQUAL, "=".to_string()),
+        '<' if is_token_match(scanner, '=') => add_token(scanner, TokenType::LESS_EQUAL, "<=".to_string()),
+        '<' => add_token(scanner, TokenType::LESS, "<".to_string()),
+        '>' if is_token_match(scanner, '=') => add_token(scanner, TokenType::LESS_EQUAL, ">=".to_string()),
+        '>' => add_token(scanner, TokenType::LESS, ">".to_string()),
+        other => add_token(scanner, TokenType::STRING, other.to_string()),
+        /* add later when all the other things get added to the match
+        other => {
+            scanner.had_error = true;
+            let e = LoxError {
+                line: scanner.line, 
+                error_type: "Some kind of error".to_owned(), 
+                error_message: format!("Incorrect token: {}", other)};
+            e.report_error();
+        }
+        */
     }
 }
 
@@ -74,7 +94,22 @@ fn advance(scanner: &mut Scanner) -> char {
     c
 }
 
-fn add_token(scanner: &mut Scanner, token_type: TokenType, token: char) {
+fn is_token_match(scanner: &mut Scanner, expected: char) -> bool {
+    if is_at_end(scanner) {
+        return false;
+    }
+    if peek(scanner) == expected {
+        advance(scanner);
+        return true;
+    }
+    return false;
+}
+
+fn peek(scanner: &Scanner) -> char {
+    scanner.source[scanner.current + 1] as char
+}
+
+fn add_token(scanner: &mut Scanner, token_type: TokenType, token: String) {
     let text = match std::str::from_utf8(&scanner.source[scanner.start .. scanner.current]){
         Ok(s) => s.to_owned(),
         Err(e) => panic!("Error at line {}: {}", scanner.line, e),
